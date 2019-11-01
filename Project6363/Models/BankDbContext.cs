ef22281205ed71dd81c3efee3617172dcce44e88
Dinesh Ghimire <dineshg822@gmail.com>
2019-11-01 13:06:40 -0400

    First commit

---
+++ Project6363/Models/BankDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Project6363.Models;
+
+
+namespace Project6363.Models
+{
+    public class BankDbContext: DbContext
+    {
+
+
+        public BankDbContext(DbContextOptions<BankDbContext> context)
+              : base(context)
+        {
+
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"data source=.\SQLEXPRESS;initial catalog=Project1;integrated security=True;MultipleActiveResultSets=True;");
+            }
+        }
+
+      
+        public DbSet<Customer> Customer { get; set; }
+
+
+
+    }
+}
---
+++ Project6363/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project6363.Models
+{
+    public class Customer
+    {
+        
+            [Required]
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+
+        public string firstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Email Address")]
+        public string email { get; set; }
+        //[DataType(phonenum)]
+        [Display(Name = "Phone Number")]
+
+        [DataType(DataType.PhoneNumber)]
+        public long phoneNumber { get; set; }
+        [Display(Name = "Address")]
+        public string address { get; set; }
+
+
+      
+    }
+}
